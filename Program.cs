0aea15f19903a1798d3de82b99f538176d71e5fb
Jeevasurya Jeyakrishnan <jeeevasuriya@gmail.com>
2025-01-27 11:47:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class GenPassword
+{
+    public static void Main(String[] args)
+    {
+        Random rand = new Random();
+
+        string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+        string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int charsize = 3;
+        string Number = "1234567890";
+        string SpeChar = "!@#$%^&*()";
+        int size = 1;
+        string randpasss = "";
+        
+        for(int i = 0; i < charsize; i++)
+        {
+            int small = rand.Next(Lowercase.Length);
+            int caps = rand.Next(Uppercase.Length);
+            randpasss += Lowercase[small];
+            randpasss += Uppercase[caps];
+
+        }
+
+        for(int i=0;i<size;i++)
+        {
+            
+            int num =rand.Next(Number.Length);
+            int spechar = rand.Next(SpeChar.Length);
+            
+            randpasss += Number[num];
+            randpasss += SpeChar[spechar];
+
+        }
+        Console.WriteLine(randpasss);
+    }
+}
